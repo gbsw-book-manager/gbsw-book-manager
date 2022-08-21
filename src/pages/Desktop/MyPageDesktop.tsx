f8135961e818ce7a5058ecfba589592d7daeb55c
jinhyo-dev <jinhyo0850@gmail.com>
2022-08-21 17:33:15 +0900

    few design changed

--- src/pages/Desktop/MyPageDesktop.tsx
+++ src/pages/Desktop/MyPageDesktop.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SideBar from "../../components/SideBar";
 import '../../styles/MyPage.css'
 
 const MyPageDesktop = () => {
+  const [currentPassword, setCurrentPassword] = useState<string>('')
+  const [newPassword, setNewPassword] = useState<string>('')
+  const [checkPassword, setCheckPassword] = useState<string>('')
+
+  const [currentPasswordIsTrue, setCurrentPasswordIsTrue] = useState<boolean>(true)
+  const [newPasswordOverFour, setNewPasswordOverFour] = useState<boolean>(true)
+  const [passwordIsEqual, setPasswordIsEqual] = useState<boolean>(true)
+
   let user = JSON.parse(localStorage.getItem('user') || '{}')
+
+  const changePassword = () => {
+    if (currentPassword.length === 0) {
+      setCurrentPasswordIsTrue(false)
+    }
+
+    if (newPassword.length < 4) {
+      setNewPasswordOverFour(false)
+    }
+
+    if (newPassword !== checkPassword) {
+      setPasswordIsEqual(false)
+    }
+
+  }
 
   return (
     <div>
@@ -44,10 +67,53 @@ const MyPageDesktop = () => {
                 <td className={'tableValue'}>
                   <div className={'passwordChangeContainer'}>
                     <div style={{marginTop: '20px'}}/>
-                    <div> <span>현재 비밀번호 </span> <input style={{marginLeft: '35px'}}/> <br/></div>
-                    <div style={{ marginTop: '9px' }}> <span>새 비밀번호 </span> <input style={{marginLeft: '48px'}}/> <br/></div>
-                    <div style={{ marginTop: '9px' }}><span>비밀번호 다시 입력 </span> <input style={{marginLeft: '5px'}}/> <br/></div>
-                    <button className={'confirmBtn'}>비밀번호 변경</button>
+                    <div>
+                      <span>현재 비밀번호 </span>
+                      <input style={{marginLeft: '35px'}} type={'password'}
+                             onChange={(e) => setCurrentPassword(e.target.value)}/>
+                      <br/>
+                    </div>
+
+                    {
+                      currentPasswordIsTrue === false && (
+                        <div className={'alertMessage'}>
+                          현재 비밀번호를 입력하세요.
+                        </div>
+                      )
+                    }
+
+                    <div style={{marginTop: '9px'}}>
+                      <span>새 비밀번호 </span>
+                      <input style={{marginLeft: '48px'}} type={'password'}
+                             onChange={(e) => setNewPassword(e.target.value)}/>
+                      <br/>
+                    </div>
+
+                    {
+                      newPasswordOverFour === false && (
+                        <div className={'alertMessage'}>
+                          비밀번호는 4자 이상이어야 합니다.
+                        </div>
+                      )
+                    }
+
+                    <div style={{marginTop: '9px'}}>
+                      <span>비밀번호 다시 입력 </span>
+                      <input style={{marginLeft: '5px'}} type={'password'}
+                             onChange={(e) => setCheckPassword(e.target.value)}/>
+                      <br/>
+                    </div>
+
+                    {
+                      passwordIsEqual === false && (
+                        <div className={'alertMessage'}>
+                          새 비밀번호와 일치하지 않습니다.
+                        </div>
+                      )
+                    }
+
+                    <button className={'confirmBtn'} onClick={changePassword}>비밀번호 변경</button>
+
                     <div style={{marginBottom: '20px'}}/>
                   </div>
                 </td>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -11,6 +11,7 @@ const SignUp = () => {
   const [password, setPassword] = useState<string>('')
   const [code, setCode] = useState<string>('')
   const [clicked, setClicked] = useState<boolean>(false)
+  const [passwordISOverFour, setPasswordIsOverFour] = useState<boolean>(true)
 
   const checkStudentId = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {value} = e.target
@@ -53,23 +54,27 @@ const SignUp = () => {
   }
 
   const loginHandler = async () => {
-    let data = {
-      "studentId": studentId,
-      "name": name,
-      "username": email,
-      "password": password,
-      "code": code
+    if (password.length < 4) {
+      setPasswordIsOverFour(false)
+    } else {
+      let data = {
+        "studentId": studentId,
+        "name": name,
+        "username": email,
+        "password": password,
+        "code": code
+      }
+      axios
+        .post('http://localhost:8080/api/sign-up', JSON.stringify(data), {
+          headers: {
+            "Content-Type": `application/json`,
+          },
+        })
+        .then((res: AxiosResponse<any>) => {
+          alert('회원가입이 완료되었습니다.')
+          window.location.replace('/')
+        });
     }
-    axios
-      .post('http://localhost:8080/api/sign-up', JSON.stringify(data), {
-        headers: {
-          "Content-Type": `application/json`,
-        },
-      })
-      .then((res: AxiosResponse<any>) => {
-        alert('회원가입이 완료되었습니다.')
-        window.location.replace('/')
-      });
   }
 
   return (
@@ -78,95 +83,107 @@ const SignUp = () => {
       <PagesLogo/>
       <div className='logoTitle' style={{fontSize: '15px', marginTop: '4px'}}>Gbsw Book Manager</div>
 
-      <input
-        className="form username-form"
-        placeholder="이름"
-        type="text"
-        name="studentname"
-        required
-        style={{marginTop: '40px'}}
-        value={name}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-        onKeyPress={handleOnKeyPress}
-      />
+      <div className={'signUpContainer'}>
+        <input
+          className="form username-form"
+          placeholder="이름"
+          type="text"
+          name="studentname"
+          required
+          maxLength={4}
+          value={name}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+          onKeyPress={handleOnKeyPress}
+        />
 
-      <input
-        className="form user-student-id"
-        placeholder="학번"
-        name="student-id"
-        required
-        maxLength={4}
-        value={studentId}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          checkStudentId(e)
-          setStudentId(e.target.value)
-        }}
-        style={{marginTop: '20px'}}
-        onKeyPress={handleOnKeyPress}
-      />
+        <input
+          className="form user-student-id"
+          placeholder="학번"
+          name="student-id"
+          required
+          maxLength={4}
+          value={studentId}
+          onChange={checkStudentId}
+          style={{marginTop: '20px'}}
+          onKeyPress={handleOnKeyPress}
+        />
 
-      <div className={'emailForm'}>
+        <div className={'emailForm'}>
 
-        <div className={'emailInputTag'}>
-          <input
-            className="form signup-userid-form"
-            placeholder="email"
-            type="text"
-            name="email"
-            required
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              checkEmail(e)
-              setEmail(e.target.value)
-            }}
-            onKeyPress={handleOnKeyPress}
-          />
-          <button className={'getCodeBtn'} onClick={isEmailFilled}>인증번호 받기</button>
+          <div className={'emailInputTag'}>
+            <input
+              className="form signup-userid-form"
+              placeholder="email"
+              type="text"
+              name="email"
+              required
+              value={email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                checkEmail(e)
+                setEmail(e.target.value)
+              }}
+              onKeyPress={handleOnKeyPress}
+            />
+            <button className={'getCodeBtn'} onClick={isEmailFilled}>인증번호 받기</button>
+          </div>
+
+          {
+            isEmailOkay === false && (
+              <div style={{
+                marginTop: '3px',
+                marginLeft: '3px',
+                color: '#e12a2a',
+                fontSize: '9px'
+              }}>이메일 형식이 올바르지 않습니다.</div>
+            )
+          }
+
         </div>
 
+
         {
-          isEmailOkay === false && (
-            <div style={{
-              marginTop: '3px',
-              marginLeft: '3px',
-              color: '#e12a2a',
-              fontSize: '9px'
-            }}>이메일 형식이 올바르지 않습니다.</div>
+          clicked === true && (
+            <input
+              className="form code-form"
+              placeholder="인증번호 (3분내로 입력)"
+              name="code"
+              maxLength={6}
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              style={{marginTop: '2px', marginBottom: '20px'}}
+            />
           )
         }
 
+        <input
+          className="form password-form"
+          placeholder="password"
+          type="password"
+          name="password"
+          required
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          onKeyPress={handleOnKeyPress}
+        />
+
+        {
+          passwordISOverFour === false && (
+            <div style={{
+              fontSize: '13px',
+              marginLeft: '2px',
+              marginTop: '3px',
+              color: '#d72020'
+            }}>비밀번호는 4자 이상이어야 합니다.</div>
+          )
+        }
+        <button className="signupButton" onClick={loginHandler}>회원가입</button>
+        <div style={{textAlign: 'center', marginTop: '20px', fontSize: '14px', marginBottom: '15vh'}}>아이디가 있으신가요? <a
+          href={'/login'}>로그인</a>
+        </div>
+
       </div>
 
 
-      {
-        clicked === true && (
-          <input
-            className="form code-form"
-            placeholder="인증번호 (3분내로 입력)"
-            name="code"
-            maxLength={6}
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-            style={{marginTop: '2px', marginBottom: '20px'}}
-          />
-        )
-      }
-
-      <input
-        className="form password-form"
-        placeholder="password"
-        type="password"
-        name="password"
-        required
-        value={password}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-        onKeyPress={handleOnKeyPress}
-      />
-
-      <button className="signupButton" onClick={loginHandler}>회원가입</button>
-      <div style={{textAlign: 'center', marginTop: '20px', fontSize: '14px', marginBottom: '15vh'}}>아이디가 있으신가요? <a
-        href={'/login'}>로그인</a>
-      </div>
     </div>
   )
 }
