56ef91e4b38357fc3574d1d0e873c328a90b8152
jinhyo-dev <jinhyo0850@gmail.com>
2022-08-28 01:31:59 +0900

    StudentManagement page done

--- src/pages/Desktop/StudentManagementDesktop.tsx
+++ src/pages/Desktop/StudentManagementDesktop.tsx
@@ -7,6 +7,7 @@ import fetcher from "../../utils/fetcher";
 import Loading from "../../components/Loading";
 import axios from "axios";
 import Swal from "sweetalert2";
+import { AiOutlineInfoCircle } from 'react-icons/ai'
 
 const StudentManagementDesktop = () => {
   const {data, error} = useSWR('http://localhost:8080/api/users', fetcher)
@@ -29,13 +30,19 @@ const StudentManagementDesktop = () => {
     axios.
       get(`http://localhost:8080/api/user?id=${id}`)
       .then((res) => {
+        let books: string = ''
+        for (let i = 0; i < res.data.length; i++) {
+          books += res.data[i].title + ', '
+        }
+
+        books = books.slice(0, -1);
+        books = books.slice(0, -1);
+
         Swal.fire({
           title: `${name}님의 대출 도서 정보`,
-          text: res.data,
-          confirmButtonText: '확인'
+          text: `도서 목록 : ${books}`,
+          confirmButtonText: '확인',
         })
-          // Swal.fire(`${name}님의 대출 도서 정보`, `<div>${res.data.title}</div>`, 'success')
-        console.log(res.data)
       })
   }
 
@@ -74,7 +81,7 @@ const StudentManagementDesktop = () => {
                         <td>{log.name}</td>
                         <td>{log.studen_id}</td>
                         <td>{log.username}</td>
-                        <td>{log.books.length}권 <button onClick={() => getUserLoanBooks(log.name, log.id)}>상세 정보</button></td>
+                        <td>{log.books.length}권 <button onClick={() => getUserLoanBooks(log.name, log.id)} className={'moreInfo'}><AiOutlineInfoCircle style={{ marginBottom: '-3px', fontSize: '15px' }}/> </button></td>
                       </tr>
                     ))}
                     </tbody>
